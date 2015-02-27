fc3116efde42dd4484f3c8505534b7dac841d014
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-02-27 18:21:07 +0100

    Improve the tutorial

--- docs/content/railway.fsx
+++ docs/content/railway.fsx
@@ -48,94 +48,60 @@ let combinedValidation =
 
 { Name = ""; EMail = "" }
 |> combinedValidation
-
 // [fsi:val it : Chessie.ErrorHandling.Result<Request,string> =
 //  Failure ["Name must not be blank"]]
     
-//
-//
-//[<Test>]
-//let ``should find empty mail``() = 
-//    { Name = "Scott"
-//      EMail = "" }
-//    |> combinedValidation
-//    |> shouldEqual (Failure [ "Email must not be blank" ])
-//
-//[<Test>]
-//let ``should find long name``() = 
-//    { Name = "ScottScottScottScottScottScottScottScottScottScottScottScottScottScottScottScottScottScottScott"
-//      EMail = "" }
-//    |> combinedValidation
-//    |> shouldEqual (Failure [ "Name must not be longer than 50 chars" ])
-//
-//[<Test>]
-//let ``should not complain on valid data``() = 
-//    let scott = 
-//        { Name = "Scott"
-//          EMail = "scott@chessie.com" }
-//    scott
-//    |> combinedValidation
-//    |> returnOrFail
-//    |> shouldEqual scott
-//
-//let canonicalizeEmail input = { input with EMail = input.EMail.Trim().ToLower() }
-//
-//let usecase = 
-//    combinedValidation
-//    >> (lift canonicalizeEmail)
-//
-//[<Test>]
-//let ``should canonicalize valid data``() = 
-//    { Name = "Scott"
-//      EMail = "SCOTT@CHESSIE.com" }
-//    |> usecase
-//    |> returnOrFail
-//    |> shouldEqual { Name = "Scott"
-//                     EMail = "scott@chessie.com" }
-//
-//[<Test>]
-//let ``should not canonicalize invalid data``() = 
-//    { Name = ""
-//      EMail = "SCOTT@CHESSIE.com" }
-//    |> usecase
-//    |> shouldEqual (Failure [ "Name must not be blank" ])
-//
-//// a dead-end function    
-//let updateDatabase input =
-//   ()   // dummy dead-end function for now
-//
-//
-//let log logF twoTrackInput = 
-//    let success(x,msgs) = logF "DEBUG. Success so far."; Success(x,msgs)
-//    let failure msgs = logF <| sprintf "ERROR. %A" msgs; Failure(msgs)
-//    either success failure twoTrackInput 
-//
-//let usecase2 logF = 
-//    usecase
-//    >> (successTee updateDatabase)
-//    >> (log logF)
-//
-//[<Test>]
-//let ``should log valid data``() = 
-//    let logF s =
-//        if s <> "DEBUG. Success so far." then
-//            failwithf "unexpected log: %s"  s
-//
-//    { Name = "Scott"
-//      EMail = "SCOTT@CHESSIE.com" }
-//    |> usecase2 logF
-//    |> returnOrFail
-//    |> shouldEqual { Name = "Scott"
-//                     EMail = "scott@chessie.com" }
-//
-//
-//[<Test>]
-//let ``should log invalid data``() = 
-//    let logF s =
-//        if s <> """ERROR. ["Email must not be blank"]""" then
-//            failwithf "unexpected log: %s"  s
-//
-//    { Name = "Scott"
-//      EMail = "" }
-//    |> usecase2 logF
-//    |> ignore
+{ Name = "Scott"; EMail = "" }
+|> combinedValidation
+// [fsi:val it : Chessie.ErrorHandling.Result<Request,string> =
+//  Failure ["Email must not be blank"]]
+
+{ Name = "ScottScottScottScottScottScottScottScottScottScottScottScottScottScottScottScottScottScottScott"
+  EMail = "" }
+|> combinedValidation
+// [fsi:val it : Chessie.ErrorHandling.Result<Request,string> =
+//  Failure ["Name must not be longer than 50 chars" ]]
+
+{ Name = "Scott"; EMail = "scott@chessie.com" }
+|> combinedValidation
+|> returnOrFail
+// [fsi:val it : Request = {Name = "Scott"; EMail = "scott@chessie.com";}]
+
+
+let canonicalizeEmail input = { input with EMail = input.EMail.Trim().ToLower() }
+
+let usecase = 
+    combinedValidation
+    >> (lift canonicalizeEmail)
+
+{ Name = "Scott"; EMail = "SCOTT@CHESSIE.com" }
+|> usecase
+|> returnOrFail
+// [fsi:val it : Request = {Name = "Scott"; EMail = "scott@chessie.com";}]
+
+{ Name = ""; EMail = "SCOTT@CHESSIE.com" }
+|> usecase
+// [fsi:val it : Result<Request,string> = Failure ["Name must not be blank"]]    
+
+// a dead-end function    
+let updateDatabase input =
+   ()   // dummy dead-end function for now
+
+
+let log twoTrackInput = 
+    let success(x,msgs) = printfn "DEBUG. Success so far."; Success(x,msgs)
+    let failure msgs = printf "ERROR. %A" msgs; Failure(msgs)
+    either success failure twoTrackInput 
+
+let usecase2 = 
+    usecase
+    >> (successTee updateDatabase)
+    >> log
+
+
+{ Name = "Scott"; EMail = "SCOTT@CHESSIE.com" }
+|> usecase2
+|> returnOrFail
+// [fsi:DEBUG. Success so far.
+//val it : Request = {Name = "Scott";
+//                    EMail = "scott@chessie.com";}]
