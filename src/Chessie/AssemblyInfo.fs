16a513e0a221d3d30c107c4ef2182abece84a1c3
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-03-10 17:44:50 +0100

    Bump version to 0.0.17

--- src/Chessie/AssemblyInfo.fs
+++ src/Chessie/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Chessie")>]
 [<assembly: AssemblyProductAttribute("Chessie")>]
 [<assembly: AssemblyDescriptionAttribute("Railway-oriented programming for .NET")>]
-[<assembly: AssemblyVersionAttribute("0.0.16")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.16")>]
+[<assembly: AssemblyVersionAttribute("0.0.17")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.17")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.16"
+    let [<Literal>] Version = "0.0.17"
