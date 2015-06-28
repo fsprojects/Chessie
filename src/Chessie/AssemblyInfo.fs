7c31c30f70b1fb12401471642904c3d17f58c9cc
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-06-28 17:55:14 +0200

    Update dependencies

--- src/Chessie/AssemblyInfo.fs
+++ src/Chessie/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Chessie")>]
 [<assembly: AssemblyProductAttribute("Chessie")>]
 [<assembly: AssemblyDescriptionAttribute("Railway-oriented programming for .NET")>]
-[<assembly: AssemblyVersionAttribute("0.1.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.1")>]
+[<assembly: AssemblyVersionAttribute("0.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.1"
+    let [<Literal>] Version = "0.2.0"
