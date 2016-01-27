7b9c724464a74ecc9b91ac4c612d8be7703db8d7
kellerd <danny.keller@tc.gc.ca>
2016-01-27 08:59:38 -0500

    Create test project

---
+++ tests/Chessie.VisualBasic.Tests/ExtensionsTests.vb
@@ -0,0 +1,91 @@
+
+Imports System.Collections.Generic
+Imports System.Linq
+Imports Chessie.ErrorHandling
+Imports Chessie.ErrorHandling.CSharp
+Imports Chessie.ErrorHandling.CSharp.ResultExtensions
+Imports Microsoft.FSharp.Core
+Imports NUnit.Framework
+
+Namespace Chessie.CSharp.Test
+    <TestFixture>
+    Public Class ExtensionsTests
+        <Test>
+        Public Sub JoinToResultsOfSuccessWorks()
+            Dim result1 = Result(Of Integer, String).Succeed(1, "added one")
+            Dim result2 = Result(Of Integer, String).Succeed(2, "added two")
+
+            Dim result3 = result1.Join(result2, Function(i) 0, Function(i) (-0), Function(i1, i2) i1 + i2)
+
+            result3.Match(ifSuccess:=Sub(x, msgs)
+                                         Assert.AreEqual(3, x)
+                                         Assert.That(msgs, [Is].EquivalentTo({"added one", "added two"}))
+                                     End Sub, ifFailure:=Sub(errs) Assert.Fail())
+        End Sub
+
+        <Test>
+        Public Sub Test()
+            Dim f As Func(Of Result(Of String, String), Result(Of String, String), Result(Of String, String), Result(Of String, String)) = Function(r1, r2, r3) From a In r1 From b In r2 From c In r3 Select a + b + c
+
+            f(Result(Of String, String).Succeed("1"), Result(Of String, String).Succeed("2"), Result(Of String, String).Succeed("3")).Match(ifSuccess:=Sub(s, list) Assert.That(s, [Is].EqualTo("123")), ifFailure:=Sub(list) Assert.Fail("should not fail"))
+
+            f(Result(Of String, String).Succeed("1", "msg1"), Result(Of String, String).Succeed("2", "msg2"), Result(Of String, String).Succeed("3", "msg3")).Match(ifSuccess:=Sub(s, list)
+                                                                                                                                                                                   Assert.That(s, [Is].EqualTo("123"))
+                                                                                                                                                                                   Assert.That(list, [Is].EquivalentTo({"msg1", "msg2", "msg3"}))
+                                                                                                                                                                               End Sub, ifFailure:=Sub(list) Assert.Fail("should not fail"))
+
+            f(Result(Of String, String).FailWith("fail"), Result(Of String, String).Succeed("2"), Result(Of String, String).Succeed("3")).Match(ifSuccess:=Sub(s, list) Assert.Fail("should fail"), ifFailure:=Sub(list) Assert.That(list, [Is].EquivalentTo({"fail"})))
+
+            f(Result(Of String, String).Succeed("1"), Result(Of String, String).FailWith("fail"), Result(Of String, String).Succeed("3")).Match(ifSuccess:=Sub(s, list) Assert.Fail("should fail"), ifFailure:=Sub(list) Assert.That(list, [Is].EquivalentTo({"fail"})))
+
+            f(Result(Of String, String).Succeed("1"), Result(Of String, String).FailWith("fail1"), Result(Of String, String).FailWith("fail2")).Match(ifSuccess:=Sub(s, list) Assert.Fail("should fail"), ifFailure:=Sub(list) Assert.That(list, [Is].EquivalentTo({"fail1"})))
+        End Sub
+
+        <Test>
+        Public Sub ToResultOnSomeShouldSucceed()
+            Dim opt = FSharpOption(Of Integer).Some(42)
+            Dim result = opt.ToResult("error")
+            result.Match(ifSuccess:=Sub(x, msgs)
+                                        Assert.AreEqual(42, x)
+                                        Assert.That(msgs, [Is].Empty)
+
+                                    End Sub, ifFailure:=Sub(errs) Assert.Fail())
+        End Sub
+
+        <Test>
+        Public Sub ToResultOnNoneShoulFail()
+            Dim opt = FSharpOption(Of Integer).None
+            Dim result = opt.ToResult("error")
+            result.Match(ifSuccess:=Sub(x, list) Assert.Fail(), ifFailure:=Sub(errs) Assert.That(errs, [Is].EquivalentTo({"error"})))
+        End Sub
+
+        <Test>
+        Public Sub MapFailureOnSuccessShouldReturnSuccess()
+            Result(Of Integer, String).Succeed(42, "warn1").MapFailure(Function(list) {42}).Match(ifSuccess:=Sub(v, msgs)
+                                                                                                                 Assert.AreEqual(42, v)
+                                                                                                                 Assert.That(msgs, [Is].Empty)
+
+                                                                                                             End Sub, ifFailure:=Sub(errs) Assert.Fail())
+        End Sub
+
+        <Test>
+        Public Sub MapFailureOnFailureShouldMapOverError()
+            Result(Of Integer, String).FailWith({"err1", "err2"}).MapFailure(Function(list) {42}).Match(ifSuccess:=Sub(v, msgs) Assert.Fail(), ifFailure:=Sub(errs) Assert.That(errs, [Is].EquivalentTo({42})))
+        End Sub
+
+        <Test>
+        Public Sub MapFailureOnFailureShouldMapOverListOfErrors()
+            Result(Of Integer, String).FailWith({"err1", "err2"}).MapFailure(Function(errs) errs.[Select](Function(err)
+                                                                                                              Select Case err
+                                                                                                                  Case "err1"
+                                                                                                                      Return 42
+                                                                                                                  Case "err2"
+                                                                                                                      Return 43
+                                                                                                                  Case Else
+                                                                                                                      Return 0
+                                                                                                              End Select
+
+                                                                                                          End Function)).Match(ifSuccess:=Sub(v, msgs) Assert.Fail(), ifFailure:=Sub(errs) Assert.That(errs, [Is].EquivalentTo({42, 43})))
+        End Sub
+    End Class
+End Namespace
---
+++ tests/Chessie.VisualBasic.Tests/SimpleValidation.vb
@@ -0,0 +1,149 @@
+﻿
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports Chessie.ErrorHandling
+Imports Chessie.ErrorHandling.CSharp
+Imports Chessie.ErrorHandling.CSharp.ResultExtensions
+
+Imports NUnit.Framework
+
+Public Class Request
+    Public Property Name() As String
+        Get
+            Return m_Name
+        End Get
+        Set
+            m_Name = Value
+        End Set
+    End Property
+    Private m_Name As String
+    Public Property EMail() As String
+        Get
+            Return m_EMail
+        End Get
+        Set
+            m_EMail = Value
+        End Set
+    End Property
+    Private m_EMail As String
+End Class
+
+Public Class Validation
+        Public Shared Function ValidateInput(input As Request) As ErrorHandling.Result(Of Request, String)
+            If input.Name = "" Then
+                Return ErrorHandling.Result(Of Request, String).FailWith("Name must not be blank")
+            End If
+            If input.EMail = "" Then
+                Return ErrorHandling.Result(Of Request, String).FailWith("Email must not be blank")
+            End If
+            Return ErrorHandling.Result(Of Request, String).Succeed(input)
+
+        End Function
+    End Class
+
+    <TestFixture>
+    Public Class TrySpecs
+        <Test>
+        Public Sub TryWillCatch()
+            Dim exn = New Exception("Hello World")
+        Dim result = ErrorHandling.Result(Of String, Exception).[Try](Function()
+                                                                          Throw exn
+
+                                                                      End Function)
+        Assert.AreEqual(exn, result.FailedWith().First())
+        End Sub
+
+        <Test>
+        Public Sub TryWillReturnValue()
+            Dim result = ErrorHandling.Result(Of String, Exception).[Try](Function()
+                                                                              Return "hello world"
+
+                                                                          End Function)
+            Assert.AreEqual("hello world", result.SucceededWith())
+        End Sub
+    End Class
+
+    <TestFixture>
+    Public Class SimpleValidation
+        <Test>
+        Public Sub CanCreateSuccess()
+            Dim request = New Request() With {
+                .Name = "Steffen",
+                .EMail = "mail@support.com"
+            }
+            Dim result = Validation.ValidateInput(request)
+            Assert.AreEqual(request, result.SucceededWith())
+        End Sub
+    End Class
+
+    <TestFixture>
+    Public Class SimplePatternMatching
+        <Test>
+        Public Sub CanMatchSuccess()
+            Dim request = New Request() With {
+                .Name = "Steffen",
+                .EMail = "mail@support.com"
+            }
+            Dim result = Validation.ValidateInput(request)
+        result.Match(Sub(x, msgs)
+                         Assert.AreEqual(request, x)
+
+                     End Sub, Sub(msgs)
+                                  Throw New Exception("wrong match case")
+
+                              End Sub)
+    End Sub
+
+        <Test>
+        Public Sub CanMatchFailure()
+            Dim request = New Request() With {
+                .Name = "Steffen",
+                .EMail = ""
+            }
+            Dim result = Validation.ValidateInput(request)
+        result.Match(Sub(x, msgs)
+                         Throw New Exception("wrong match case")
+
+                     End Sub, Sub(msgs)
+                                  Assert.AreEqual("Email must not be blank", msgs(0))
+                              End Sub)
+    End Sub
+    End Class
+
+    <TestFixture>
+    Public Class SimpleEitherPatternMatching
+        <Test>
+        Public Sub CanMatchSuccess()
+            Dim request = New Request() With {
+                .Name = "Steffen",
+                .EMail = "mail@support.com"
+            }
+            Dim result = Validation.ValidateInput(request).Either(Function(x, msgs)
+                                                                      Return x
+
+                                                                  End Function, Function(msgs)
+                                                                                    Throw New Exception("wrong match case")
+
+                                                                                End Function)
+            Assert.AreEqual(request, result)
+        End Sub
+
+        <Test>
+        Public Sub CanMatchFailure()
+            Dim request = New Request() With {
+                .Name = "Steffen",
+                .EMail = ""
+            }
+            Dim result = Validation.ValidateInput(request).Either(Function(x, msgs)
+                                                                      Throw New Exception("wrong match case")
+
+                                                                  End Function, Function(msgs)
+                                                                                    Return msgs(0)
+
+                                                                                End Function)
+
+            Assert.AreEqual("Email must not be blank", result)
+        End Sub
+    End Class
