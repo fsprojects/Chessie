2630d3f8cad53dfb60f49326c80d26e8fb5de16a
Enrico Sada <enrico@sada.io>
2016-03-08 16:15:43 +0100

    convert Chessie.CSharp.Test

---
+++ tests/Chessie.CSharp.Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace Chessie.CSharp.TestRunner
+{
+    public static class Program
+    {
+        public static int Main(string[] argv)
+        {
+#if DNXCORE50
+            var run = new NUnitLite.AutoRun(typeof(Program).GetTypeInfo().Assembly);
+            return run.Execute(argv, (new NUnit.Common.ExtendedTextWrapper(Console.Out)), Console.In);
+#else
+            var run = new NUnitLite.AutoRun();
+            return run.Execute(argv);
+#endif
+        }
+    }
+}
