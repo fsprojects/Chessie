0b6d0b7b1b9dd61706ef587612548bc9573876c9
Enrico Sada <enrico@sada.io>
2016-03-08 16:15:42 +0100

    nunit 3 use nunitlite to run test suite, the test project are executables

---
+++ tests/Chessie.Tests/Program.fs
@@ -0,0 +1,17 @@
+﻿module Chessie.TestRunner
+
+open System
+open System.Reflection
+
+type Program = class end
+
+[<EntryPoint>]
+let main argv = 
+
+#if DNXCORE50
+    let run = typeof<Program>.GetTypeInfo().Assembly |> NUnitLite.AutoRun
+    run.Execute(argv, (new NUnit.Common.ExtendedTextWrapper(Console.Out)), Console.In)
+#else
+    let run = NUnitLite.AutoRun()
+    run.Execute(argv)
+#endif
