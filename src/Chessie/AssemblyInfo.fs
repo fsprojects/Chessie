f0237b5d23da6dbc33d8aab59c8be947f6878dfb
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-07-21 16:23:27 +0200

    Bump version to 0.6.0

--- src/Chessie/AssemblyInfo.fs
+++ src/Chessie/AssemblyInfo.fs
@@ -5,11 +5,11 @@ open System.Runtime.CompilerServices
 [<assembly: AssemblyTitleAttribute("Chessie")>]
 [<assembly: AssemblyProductAttribute("Chessie")>]
 [<assembly: AssemblyDescriptionAttribute("Railway-oriented programming for .NET")>]
-[<assembly: AssemblyVersionAttribute("0.5.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.5.0")>]
+[<assembly: AssemblyVersionAttribute("0.6.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.6.0")>]
 [<assembly: ExtensionAttribute()>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.5.0"
-    let [<Literal>] InformationalVersion = "0.5.0"
+    let [<Literal>] Version = "0.6.0"
+    let [<Literal>] InformationalVersion = "0.6.0"
